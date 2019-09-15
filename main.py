830b661820fb0ed68c9a6770af85ff8349d8e6a0
iFuunyMan520 <ifuunyman520@gmail.com>
2019-09-15 17:42:48 +0300

    pr1.1

--- main.py
+++ main.py
@@ -6,8 +6,29 @@ app = Flask(__name__)
 @app.route('/phone/', methods=['post', 'get'])
 def phone():
 
-    message = request.form.get('phone')
-    return render_template('phone.html', message=message)
+    return render_template('phone.html')
+
+
+@app.route('/phone/confirm_code', methods=['post', 'get'])
+def confirm():
+
+    phone_number = request.form.get('phone')
+
+    if phone_number:
+        return render_template('confirm_code.html')
+    else:
+        return 'Error'
+
+
+@app.route('/phone/confirm_code/confirming', methods=['post', 'get'])
+def confirming():
+
+    confirm_code = request.form.get('confirm')
+
+    if confirm_code:
+        return 'Ok'
+    else:
+        return 'Error'
 
 
 if __name__ == "__main__":
