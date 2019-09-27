3494b0672fe2178f52d25f9d2428e406b6543d56
iFuunyMan520 <ifuunyman520@gmail.com>
2019-09-27 14:12:56 +0300

    v1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from models.test_db import session, User, Flask, render_template, request
+from flask import Response
 import random
 
 app = Flask(__name__)
@@ -13,25 +14,38 @@ def phone():
 
 @app.route('/phone/confirm', methods=['post', 'get'])
 def confirm():
-    phone_number = request.form.get('phone')
-    your_phone = session.query(User).filter_by(phone=phone_number).first()
-    if not your_phone:
-        user1 = User(phone=phone_number, code=Random)
-        session.add(user1)
-        session.commit()
-    return render_template('confirm_code.html')
+    resp = Response(status=200, response=render_template('confirm_code.html'))
+    if request.method == 'POST':
+        phone_number = request.form.get('phone')
+        if not phone_number:
+            return 'You didn`t enter your phone number. Try it again'
+        else:
+            resp.set_cookie('phone', phone_number)
+            your_phone = session.query(User).filter_by(phone=phone_number).first()
+            if not your_phone:
+                user1 = User(phone=phone_number, code=Random)
+                session.add(user1)
+                session.commit()
+            else:
+                your_phone.code = Random
+                session.add(your_phone)
+                session.commit()
+            return resp
+    else:
+        return 'Go to the first page and enter your phone number'
 
 
 @app.route('/phone/confirm/enter', methods=['post', 'get'])
 def enter():
-    conf = request.form.get('confirm')
-    phone_number = request.form.get('phone')
-    your_phone = session.query(User).filter_by(phone=phone_number).first()
-    #if your_phone.code == conf:
-        #return 'OK'
-    #else:
-        #return your_phone.code
-    return phone_number
+    if request.method == 'POST':
+        conf = request.form.get('confirm')
+        your_phone = session.query(User).filter_by(phone=request.cookies.get('phone')).first()
+        if your_phone.code == conf:
+            return 'OK'
+        else:
+            return Response(status=200, response=b'Try again')
+    else:
+        return 'Go to the first page and enter your phone number'
 
 
 if __name__ == "__main__":
