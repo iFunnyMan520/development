a91ca32b23d7ea3daf6c67a487db891c37d8db8a
iFuunyMan520 <ifuunyman520@gmail.com>
2019-12-03 20:53:57 +0200

    adding data from https://autodiler.me/

--- request_test.py
+++ request_test.py
@@ -1,6 +1,7 @@
 import requests
 from components.users.models import User
 from components.advert.models import Car, Advt
+from server import db, app
 
 response = requests.post('https://weblabapi.com/ads/list',
                          data={'limit': 36, 'page': 1, 'sortBy': "dateDesc",
@@ -9,21 +10,33 @@ response = requests.post('https://weblabapi.com/ads/list',
 commit_data = response.json()
 
 model = ''
+i = 0
 count = 0
 
 while count < 35:
     seo = commit_data['paidAds'][count]['seo']
-
+    count += 1
     r = requests.post('https://weblabapi.com/ads/getBySeo', data={'seo': seo})
     c_data = r.json()
-    brand = c_data['ad']['brand']
-    if list(c_data['ad'].keys())[27] == 'product':
+
+    if list(c_data['ad'].keys())[7] == 'brand':
+        brand = c_data['ad']['brand']['name']
+    if list(c_data['ad'].keys())[8] == 'product':
         model = c_data['ad']['product']['model']
-    title = c_data['ad']['title']
+    title = c_data['ad']['titleCompiled']
     description = c_data['ad']['description']
     phone = c_data['ad']['user']['phones'][0]['phone']
-    user_id = c_data['ad']['user']['id']
-    count += 1
-    print(model)
+    user_name = c_data['ad']['user']['name']
+    price = c_data['ad']['price']
+
+    one = User(phone=phone, name=user_name)
+    two = Car(brand=brand, model=model)
+    three = Advt(name=title, description=description, price=price)
+
+    if __name__ == "__main__":
+        with app.app_context():
+            db.session.add_all([one, two, three])
+            db.session.commit()
+
 
 
